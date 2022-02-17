f683ac9db8fd093f865a2591e959f8d4e5409484
FernandoHeMa <193225@ids.upchiapas.edu.mx>
2022-02-17 12:44:57 -0600

    Primer versión

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,5 @@
+import 'package:actividad_17_febrero/views/LoginPage.dart';
 import 'package:flutter/material.dart';
-
-import 'Views/LoginPage.dart';
 
 void main() {
   runApp(const MyApp());
@@ -12,6 +11,10 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return LoginPage();
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'MVP 193225',
+      home:  LoginPage(),
+    );
   }
 }
---
+++ lib/views/LoginPage.dart
@@ -0,0 +1,144 @@
+import 'package:flutter/material.dart';
+
+
+class LoginPage extends StatelessWidget {
+  const LoginPage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Color(0xffffffff),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          children: [
+            // Container for SVG asset
+            Padding(
+              padding: EdgeInsets.only(top: 50.0, bottom: 0.0, left: 24, right: 24),
+              child: Container(
+                height: 150,
+                color: Colors.blue,
+              ),
+            ),
+            Column(
+              children: [
+                // Google button
+                Container(
+                  width: 300,
+                  height: 48,
+                  decoration: BoxDecoration(
+                    color: Color(0xff3169f5),
+                    borderRadius: BorderRadius.circular(40.0),
+                  ),
+                  // LOGO AND TEXT OF BUTTON
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      Icon(Icons.android, color: Colors.white),
+                      SizedBox(width: 20),
+                      Text(
+                        "Continuar con Google",
+                        style: TextStyle(
+                            color: Colors.white,
+                            fontWeight: FontWeight.bold,
+                            fontSize: 14
+                        ),
+                      )
+                    ],
+                  ),
+                ),
+                SizedBox(height: 15),
+                // Facebook button
+                Container(
+                  width: 300,
+                  height: 48,
+                  decoration: BoxDecoration(
+                    color: Color(0xff324fa5),
+                    borderRadius: BorderRadius.circular(40.0),
+                  ),
+                  // LOGO AND TEXT OF BUTTON
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      Icon(Icons.facebook, color: Colors.white),
+                      SizedBox(width: 20),
+                      Text(
+                        "Continuar con Facebook",
+                        style: TextStyle(
+                            color: Colors.white,
+                            fontWeight: FontWeight.bold,
+                            fontSize: 14
+                        ),
+                      )
+                    ],
+                  ),
+                ),
+              ],
+            ),
+
+            // Email button
+            Container(
+              width: 300,
+              height: 48,
+              decoration: BoxDecoration(
+                border: Border.all(
+                  color: Color(0xff64686f),
+                  width: 3// red as border color
+                ),
+                borderRadius: BorderRadius.circular(40.0),
+              ),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                Icon(Icons.email, color:Color(0xff64686f)),
+                  SizedBox(width: 10),
+                  Text(
+                    "Registrase con e-mail",
+                    style: TextStyle(
+                        color: Color(0xff64686f),
+                        fontWeight: FontWeight.bold,
+                        fontSize: 14
+                    ),
+                  )
+                ],
+              ),
+            ),
+             // TEXT BUTTONS
+            Column(
+              children: [
+                Text(
+                    'Entrar como invitado',
+                    style: TextStyle(color: Color(0xfffc1460), fontWeight: FontWeight.bold),
+                ),
+                SizedBox(height: 10,),
+                Text(
+                    'Entrar como vendedor',
+                    style: TextStyle(color: Color(0xff76aa757),fontWeight: FontWeight.bold),
+                  ),
+              ],
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Text(
+                  "¿Ya tienes cuenta? ",
+                  style: TextStyle(
+                    color: Color(0xff64686f)
+                  ),
+                ),
+                Text(
+                  "Iniciar sesión",
+                  style: TextStyle(
+                      color: Color(0xfffc1460),
+                      fontWeight: FontWeight.bold
+                  ),
+                )
+              ],
+            )
+
+          ],
+        ),
+      ),
+    );
+  }
+}
